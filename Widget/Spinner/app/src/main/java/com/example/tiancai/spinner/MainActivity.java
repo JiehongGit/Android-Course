4e03e1c124952843eae346257b6c37661af5e9b6
KnightGithub <974861604@qq.com>
2017-08-03 17:41:36 +0800

    Android组件系列学习

---
+++ Widget/Spinner/app/src/main/java/com/example/tiancai/spinner/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tiancai.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public final class MainActivity{
+    public static final class array{
+        public static final int countries=0x7f040000;
+    }
+    public static final class attr{
+
+    }
+    public static final class drawble{
+        public static final int icon=0x7f020000;
+    }
+    public static final class id{
+        public static final int spinnerId=0x7f060001;
+        public static final int spinner_2=0x7f060002;
+        public static final int spinnerbtn=0x7f060000;
+    }
+    public static final class layout{
+        public static final int main=0x7f030000;
+        public static final int spinner=0x7f030001;
+    }
+    public static final class string{
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int spinnerHi=0x7f050002;
+    }
+}
---
+++ Widget/SpinnerDemo/app/src/main/java/com/example/tiancai/spinnerdemo/BuildConfig.java
@@ -0,0 +1,9 @@
+package com.example.tiancai.spinnerdemo;
+
+/**
+ * Created by Tiancai on 2017/4/19.
+ */
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
